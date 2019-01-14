2230b5cc6cfbc3db819d29ca33a5250d4b3403bc
agnesuwamahoro <magretmuk@gmail.com>
2019-01-14 16:06:14 +0200

    added identation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic
 
-//Player Object Constructor
+
 function Player(name) {
 	this.name = name;
 	this.turnRunningScore = 0;
@@ -9,16 +9,16 @@ function Player(name) {
 	this.currentTurnArray = [];
   } 
   
-  //Creates our two players
+  
   var playerOne = new Player("Player One");
   var playerTwo = new Player("Player Two");
   
-  // Dice Constructor (argument = number of sides you want. default = 6)
+  
   function Dice(sides) {
 	this.sides = sides || 6;
   }
   
-  // Roll Prototype - Gets a random number between 1 and 6
+  
   Dice.prototype.roll = function() {
 	var roll = Math.floor((Math.random() * this.sides ) + 1);
 	if (roll === 1) {
@@ -27,12 +27,12 @@ function Player(name) {
 	return roll;
   }
   
-  // Current Turn Array Prototype
+  
   Player.prototype.addRollToArray = function (x) {
 	this.currentTurnArray.push(x)
   }
   
-  // Get sum of Turn Running Total (and exclude 1s from total)
+ 
   Player.prototype.sumOfRolls = function() {
 	for (var i = 0; i < this.currentTurnArray.length; i++) {
 	  if (this.currentTurnArray[i = this.currentTurnArray.length - 1] === 1) {
@@ -42,38 +42,38 @@ function Player(name) {
 	  }
 	}
   }
-  // Prototype to Bank points
+  
   Player.prototype.bankPoints = function() {
 	this.totalBankedScore += this.turnRunningScore;
   }
   
-  // Hold/Stay turnRunningScore <--Added this, score will bank when a user presses stay
+  
   Player.prototype.stayTurn = function () {
 	this.bankPoints();
 	this.turnRunningScore = 0;
   }
   
-  // Create six-sided dice
+  
   var sixSidedDice = new Dice();
   
-  //Prototype to enter last roll value into player object
+  
   Player.prototype.setLastRoll = function(x) {
 	this.lastRoll = x;
   }
   
-  // Prototype to reset running total if 1 is rolled
+  
   Player.prototype.resetRunningTotalOnOne = function() {
 	if (this.lastRoll === 1) {
 	  this.turnRunningScore = 0;
 	}
   }
   
-  // FRONT END LOGIC
+ 
   
   $(function() {
 	var playerOneTurn = true;
   
-	//PLAYER ONE
+	
 	$("#player-one-roll").on("click", function() {
 	  var animationName = 'animated tada';
 	  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -81,7 +81,7 @@ function Player(name) {
 		$(this).removeClass(animationName);
 	  });
 	});
-	//Player One Roll Button Click Event
+	
 	$("#player-one-roll").click(function(event) {
 	  event.preventDefault();
 	  var sixSidedDiceRoll = sixSidedDice.roll();
@@ -92,7 +92,7 @@ function Player(name) {
   
 	  $("#player-one-running").html("<h1 class='running-total'>" + playerOne.turnRunningScore + "</h1>");
   
-	  //Change image based on dice roll, AND swith player turn
+	  
 	  if (sixSidedDiceRoll === 1) {
 		$("#dice-pic").attr("src", "img/one.png");
 		var playerOneTurn = false;
@@ -116,7 +116,7 @@ function Player(name) {
 	  }
 	});
   
-  //Player One Stay Button Click Event
+ 
   $("#player-one-stay").click(function(event) {
 	event.preventDefault();
   
@@ -133,7 +133,7 @@ function Player(name) {
   
 	var playerOneTurn = false;
   
-	// $button set playerTwo
+	
 	  if (!playerOneTurn) {
 		 $("#player-two-buttons").show();
 		 $("#player-one-buttons").hide();
@@ -144,9 +144,9 @@ function Player(name) {
 	  }
 	});
   
-	/// PLAYER TWO
+	
   
-	//Roll Button Click Event
+	
   
 	$("#player-two-roll").click(function(event) {
 	  event.preventDefault();
@@ -158,7 +158,7 @@ function Player(name) {
   
 	  $("#player-two-running").html("<h1 class='running-total'>" + playerTwo.turnRunningScore + "</h1>");
   
-	  //Change image based on dice roll, AND swith player turn
+	  
 	  if (sixSidedDiceRoll === 1) {
 		$("#dice-pic").attr("src", "img/one.png");
 		var playerOneTurn = true;
@@ -182,7 +182,7 @@ function Player(name) {
 	  }
 	});
   
-  // Player Two animationEnd
+  
   $("#player-two-roll").on("click", function() {
 	var animationName = 'animated tada';
 	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -191,16 +191,16 @@ function Player(name) {
 	});
   });
   
-  //Player Two Stay Button Click Event
+  
   $("#player-two-stay").click(function(event) {
 	event.preventDefault();
   
-	// What happens when a user clicks the stay button
+	
 	playerTwo.stayTurn();
 	$("#player-two-running").html("<h1 class='running-total'>" + playerTwo.turnRunningScore + "</h1>");
 	$("#player-two-score").html("<h1 class='total-score'>" + playerTwo.totalBankedScore + "</h1>");
   
-	// Win condition
+	
 	if (playerTwo.totalBankedScore >= 20) {
 	  $("#winner").show();
 	  $("#winner").html("<h1 class='total-score'>" + "You win!!!" + "</h1>");
@@ -210,7 +210,7 @@ function Player(name) {
   
 	var playerOneTurn = true;
   
-	// Toggle between players
+	
 	  if (playerOneTurn) {
 		 $("#player-one-buttons").show();
 		 $("#player-two-buttons").hide();
